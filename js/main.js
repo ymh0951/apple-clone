15d5f90cc98ba99d696a62763832b1975fd8e8b0
Minho Yoo <ymh0951@naver.com>
2024-12-03 17:20:04 +0900

    인터렑티브

--- js/main.js
+++ js/main.js
@@ -114,6 +114,8 @@
         rect2X: [0, 0, { start: 0, end: 0 }],
         blendHeight: [0, 0, { start: 0, end: 0 }],
         canvas_scale: [0, 0, { start: 0, end: 0 }],
+        canvasCaption_opacity: [0, 1, { start: 0, end: 0 }],
+        canvasCaption_translateY: [20, 0, { start: 0, end: 0 }],
         rectStartY: 0,
       },
     },
@@ -148,6 +150,8 @@
   }
 
   setCanvasImages();
+
+  function checkMenu() {}
 
   function setLayout() {
     // 각 스크롤 섹션의 높이 세팅
@@ -582,6 +586,23 @@
           ) {
             objs.canvas.classList.remove("sticky");
             objs.canvas.style.marginTop = `${scrollHeight * 0.4}px`;
+
+            values.canvasCaption_opacity[2].start = values.canvas_scale[2].end;
+            values.canvasCaption_opacity[2].end =
+              values.canvasCaption_opacity[2].start + 0.1;
+            objs.canvasCaption.style.opacity = calcValues(
+              values.canvasCaption_opacity,
+              currentYOffset
+            );
+
+            values.canvasCaption_translateY[2].start =
+              values.canvas_scale[2].end;
+            values.canvasCaption_translateY[2].end =
+              values.canvasCaption_opacity[2].start + 0.1;
+            objs.canvasCaption.style.transform = `translate3d(0, ${calcValues(
+              values.canvasCaption_translateY,
+              currentYOffset
+            )}%, 0)`;
           }
         }
 
